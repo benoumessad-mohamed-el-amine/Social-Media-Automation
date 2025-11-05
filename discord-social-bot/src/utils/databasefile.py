78abdbdfe79132c6e7a19a30be64434e55554d84
souha <aknouchechaima00@gmail.com>
2025-11-05 09:10:11 +0100

    .

--- discord-social-bot/src/utils/databasefile.py
+++ discord-social-bot/src/utils/databasefile.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 
 load_dotenv()
-
+#testfetch123
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("MONGO_DB_NAME", "social_bot")
 
